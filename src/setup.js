f18699972da20be8da45574d2881ad2d3cd6763f
Jenya <leo@campus.technion.ac.il>
2019-11-05 12:43:12 +0200

    auto generated schema.graphql updated

--- src/setup.js
+++ src/setup.js
@@ -1,44 +1,75 @@
 const csvFilePath = 'data/201901-bluebikes-tripdata.csv';
 const csv = require('csvtojson');
 
-const {fieldsMapping} =  require("./consts");
+const { fieldsMapping } = require('./consts');
 
-const db = require("./neo4j.js");
+const db = require('./neo4j.js');
 db.init();
 
 (async () => {
   // create schema
-  let typeDefs = `# schema dynamically created by setup.js on ${(new Date()).toISOString()}\n\n`;
+  let typeDefs = `# schema dynamically created by setup.js on ${new Date().toISOString()}\n\n`;
 
+  let nodeProperties = `\n`;
   typeDefs += `type Node {\n`;
   for (const property of fieldsMapping.startNode) {
-    typeDefs += (`\t${property.name}: ${property.type}\n`)
+    typeDefs += `\t${property.name}: ${property.type}\n`;
+    nodeProperties += `\t\t${property.name}: ${property.type}\n`;
   }
   typeDefs += `}\n\n`;
-
 
   typeDefs += `input NodeInput {\n`;
   for (const property of fieldsMapping.startNode) {
-    typeDefs += (`\t${property.name}: ${property.type}\n`)
+    typeDefs += `\t${property.name}: ${property.type}\n`;
   }
   typeDefs += `}\n\n`;
 
-
+  let edgeProperties = `\n`;
   typeDefs += `type Edge {\n`;
   for (const property of fieldsMapping.edgeInfo) {
-    typeDefs += (`\t${property.name}: ${property.type}\n`)
+    typeDefs += `\t${property.name}: ${property.type}\n`;
+    edgeProperties += `\t\t${property.name}: ${property.type}\n`;
   }
-  typeDefs += (`\tstartNode: Node\n`);
-  typeDefs += (`\tstopNode: Node\n`);
+  typeDefs += `\tstartNode: Node\n`;
+  edgeProperties += `\t\tstartNode: Node\n`;
+  typeDefs += `\tstopNode: Node\n`;
+  edgeProperties += `\t\tstopNode: Node\n`;
 
   typeDefs += `}\n\n`;
 
-  typeDefs +=`type Mutation {\n`;
-  typeDefs += `\tCreateEdge(startNode: NodeInput, stopNode: NodeInput, startTime: String, stopTime: String): Edge\n`;
+  typeDefs += `type NodeUpdateResponse {\n`;
+  typeDefs += `\tsuccess: Boolean!\n`;
+  typeDefs += `\tmessage: String\n`;
+  typeDefs += `\tnode: Node\n`;
+  typeDefs += `}\n\n`;
+
+  typeDefs += `type EdgeUpdateResponse {\n`;
+  typeDefs += `\tsuccess: Boolean!\n`;
+  typeDefs += `\tmessage: String\n`;
+  typeDefs += `\tedge: Edge\n`;
+  typeDefs += `}\n\n`;
+
+  typeDefs += `type Query {\n`;
+  typeDefs += `\tNode(${nodeProperties}\t): [Node]\n`;
+  typeDefs += `\tEdge(${nodeProperties}\t): [Edge]\n`;
+  typeDefs += `}\n\n`;
+
+  typeDefs += `type Mutation {\n`;
+  typeDefs += `\tCreateEdge(${edgeProperties
+    .split(': Node')
+    .join(': NodeInput')}\t): EdgeUpdateResponse\n`;
+  typeDefs += `\tUpdateEdge(${edgeProperties
+    .split(': Node')
+    .join(': NodeInput')}\t): EdgeUpdateResponse\n`;
+  typeDefs += `\tDeleteEdge(id: ID!): EdgeUpdateResponse\n`; // todo how to delete edge? by what?
+  typeDefs += `\n`;
+  typeDefs += `\tCreateNode(${nodeProperties}\t): NodeUpdateResponse\n`;
+  typeDefs += `\tUpdateNode(${nodeProperties}\t): NodeUpdateResponse\n`;
+  typeDefs += `\tDeleteNode(id: ID!): NodeUpdateResponse\n`;
   typeDefs += `}`;
 
-  const fs = require("fs");
-  fs.writeFileSync("schema.graphql", typeDefs);
+  const fs = require('fs');
+  fs.writeFileSync('schema.graphql', typeDefs);
 
   const dataset = await csv().fromFile(csvFilePath);
 
@@ -46,12 +77,12 @@ db.init();
   await db.removeAllEdges();
   await db.removeAllNodes();
 
-
   const nodes = [];
 
   for (const dataElement of dataset) {
     // construct nodes
-    let startNode = {}, endNode = {};
+    let startNode = {},
+      endNode = {};
 
     for (const property of fieldsMapping.startNode) {
       startNode[property.name] = dataElement[property.dataName];
@@ -60,7 +91,6 @@ db.init();
     for (const property of fieldsMapping.endNode) {
       endNode[property.name] = dataElement[property.dataName];
     }
-
 
     // insert nodes if needed
     if (!nodes.find(node => node.id === startNode.id)) {
@@ -71,7 +101,6 @@ db.init();
       await db.insertNode(endNode);
       nodes.push(endNode);
     }
-
 
     // construct edgeInfo
     let edgeInfo = {};
