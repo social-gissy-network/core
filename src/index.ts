98f123b95405e272e9839e191401bf5b1d5d79c9
Jenya <leo@campus.technion.ac.il>
2019-11-18 13:26:05 +0200

    use process.env.PORT (heroku's port)

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const server = new ApolloServer({
 });
 
 // Specify port and path for GraphQL endpoint
-const port = process.env.GRAPHQL_LISTEN_PORT || 4001;
+const port = process.env.PORT || 4001;
 let path = '/graphql';
 
 /*
