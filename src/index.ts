69e22c96e5fa486ad051ffed0f2e96bf9ea72c58
Jenya <leo@campus.technion.ac.il>
2019-11-22 10:29:35 +0200

    - remove the use of clusters - nodemon added to devDep

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Worker } from "cluster";
 import { ApolloServer, ServerRegistration } from 'apollo-server-express';
 import express from 'express';
 import dotenv from 'dotenv';
@@ -8,108 +7,79 @@ import * as fs from "fs";
 import * as consts from "./consts"
 
 export {};
+dotenv.config();
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+// hello again
+const app = express();
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+// a middleware to inspect RAM usage - when we'll be running out of RAM - just return 500
+// and clean RAM
+app.all('/', function (req, res, next) {
+  setInterval(() => {
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    const total = process.memoryUsage().heapTotal / 1024 / 1024;
+    const usedMB = Math.round(used * 100) / 100;
+    const totalMB = Math.round(total * 100) / 100;
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    if ((totalMB - usedMB) < consts.HEAP_SIZE_LEFT_THRESHOLD(totalMB)) { // todo decide about a threshold
+      res.status(500);
+      res.json({
+        "message": "Failed to fetch. Max heap size exceeded"
+      });
+    }
+  }, consts.CHECK_HEAP_INTERVAL);
+
+  next();
+});
+
+const compression = require('compression');
+let shouldCompress = (req: any, res: any) => {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
   }
 
-  cluster.on('exit', (deadWorker: Worker, code: number, signal: string) => {
-    // Restart the worker
-    let worker = cluster.fork();
+  // fallback to standard filter function
+  return compression.filter(req, res)
+};
+app.use(compression({filter: shouldCompress}));
 
-    // Note the process IDs
-    let newPID = worker.process.pid;
-    let oldPID = deadWorker.process.pid;
+let db = new DBManager();
+const typeDefs = fs.readFileSync('build/schema.graphql').toString('utf-8');
 
-    // Log the event
-    console.log('worker '+oldPID+' died.');
-    console.log('worker '+newPID+' born.');
-  });
-}
-else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  // set environment variables from ../.env
-  dotenv.config();
-
-  const app = express();
-
-  app.all('/', function (req, res, next) {
-    setInterval(() => {
-      const used = process.memoryUsage().heapUsed / 1024 / 1024;
-      const total = process.memoryUsage().heapTotal / 1024 / 1024;
-      const usedMB = Math.round(used * 100) / 100;
-      const totalMB = Math.round(total * 100) / 100;
-
-      if ((totalMB - usedMB) < consts.HEAP_SIZE_LEFT_THRESHOLD(totalMB)) { // todo decide about a threshold
-        res.status(500);
-        res.json({
-          "message": "Failed to fetch. Max heap size exceeded"
-        });
-
-        process.exit();
-      }
-    }, consts.CHECK_HEAP_INTERVAL);
-
-    next();
-  });
-
-  const compression = require('compression');
-  let shouldCompress = (req:any, res:any) => {
-    if (req.headers['x-no-compression']) {
-      // don't compress responses with this request header
-      return false
-    }
-
-    // fallback to standard filter function
-    return compression.filter(req, res)
-  };
-  app.use(compression({ filter: shouldCompress }));
-
-  let db = new DBManager();
-  const typeDefs = fs.readFileSync('build/schema.graphql').toString('utf-8');
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: { db },
-    introspection: true,
-    playground: true,
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {db},
+  introspection: true,
+  playground: true,
+});
 
 // Specify port and path for GraphQL endpoint
-  const port = process.env.PORT || 4001;
-  let path = '/';
+const port = process.env.PORT || 4001;
+let path = '/';
 
-  /*
-   * Optionally, apply Express middleware for authentication, etc
-   * This also also allows us to specify a path for the GraphQL endpoint
-   */
+/*
+ * Optionally, apply Express middleware for authentication, etc
+ * This also also allows us to specify a path for the GraphQL endpoint
+ */
 
-  let serverRegistration: ServerRegistration = { app, path };
-  serverRegistration.onHealthCheck = () => {
-    return new Promise((resolve, reject) => {
-      // Replace the `true` in this conditional with more specific checks!
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  };
-
-
-  server.applyMiddleware(serverRegistration);
-
-  app.listen({ port, path }, () => {
-    // eslint-disable-next-line
-    console.log(`Worker ${process.pid} started GraphQL server at http://localhost:${port}${path}`);
+let serverRegistration: ServerRegistration = {app, path};
+serverRegistration.onHealthCheck = () => {
+  return new Promise((resolve, reject) => {
+    // Replace the `true` in this conditional with more specific checks!
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
   });
-}
+};
+
+
+server.applyMiddleware(serverRegistration);
+
+app.listen({port, path}, () => {
+  // eslint-disable-next-line
+  console.log(`started GraphQL server at http://localhost:${port}${path}`);
+});
--- tests/neo4j-tests.js
+++ tests/neo4j-tests.js
@@ -1,7 +1,7 @@
 const { DBManager } = require('../build/neo4j');
 let db = new DBManager();
 (async () => {
-    let friends = await db.getPathsOfLengthN(1);
+    let friends = await db.getPathsOfLengthN(2, "119");
     console.log(friends);
 })
 ().catch(error => {
